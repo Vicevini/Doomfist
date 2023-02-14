77f90c86f45f55a48a708d404c959a439d4164ed
Vinicius Oliveira <vicevini@live.com>
2023-02-14 14:59:00 -0300

    services and controllers

--- controladores/controller.js
+++ controladores/controller.js
@@ -1,9 +1,9 @@
-const fs = require('fs')
+const { getTodosLivros } = require('../services/services')
 
 function getLivros(req, res) {
     try {
-        const livros = fs.readFileSync('livros.json')
-        res.send(JSON.parse(livros))
+        const livros = getTodosLivros()
+        res.send(livros)
     } catch (error) {
         res.send('Erro ao ler o arquivo')
     }
